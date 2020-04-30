20435f6a850f052b69afc6fa84d2d98b3744369a
MichaelDies <52653092+MichaelDies@users.noreply.github.com>
2020-04-30 04:44:48 +0300

    Add files via upload

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,41 +1,59 @@
 let input;
 var startTime, endTime;
-var counter = 0;
+var counter =0;
 var timeDiff;
-function setup(scale) {
+var speed;
+function setup() {
   createCanvas(20, 200);
   background(255);
-
   input = new p5.AudioIn();
   input.start();
 }
 
-function draw(draw) {
+function draw() {
   let volume = input.getLevel();
-  let threshold = 0.8;
-  if (volume > threshold) {
-    if (counter==0) {
-      setTimeout(100);
+  let threshold = 0.4;
+
+
+
+
+  if (counter == 0) {
+    if (volume > threshold) {
+      //setTimeout(100);;
       startTime = Date.now()/1000;
-      counter = 1;
+      counter =1;
+      //setTimeout(100);
 
     }
-    else {
-      endTime = Date.now()/1000;
-      counter=0;
-      volume = 0;
-      timeDiff = endTime - startTime;
-        setTimeout(100);
-    }
-  }
 
-  var speed;
-  speed = 10 / timeDiff;
+}else if (counter == 1) {
+      if (volume > threshold) {
+        endTime = Date.now()/1000;
+        counter=2;
+      //  volume = 0;
+        //setTimeout(100);
+          timeDiff = endTime - startTime;
+      }
 
-  // Graph the overall potential volume, w/ a line at the threshold
+
+    } else if (counter ==2){
+        counter=0;
+          if (timeDiff> 0) {
+            speed = 10 / timeDiff;
+            document.getElementById("calculations"). innerHTML = "Speed = " + speed + "<br>" + "Time = " + timeDiff + "<br> " + "StartTtime = " + startTime + "<br> "  + "EndTime = " + endTime  ;
+            setTimeout(100);
+            //timeDiff=0;
+
+          }
+
+      }
+
+
+
+
+
   let y = map(volume, 0, 1, height, 0);
   let ythreshold = map(threshold, 0, 1, height, 0);
-
   noStroke();
   fill(175);
   rect(0, 0, 20, height);
@@ -44,6 +62,9 @@ function draw(draw) {
   rect(0, y, 20, y);
   stroke(0);
   line(0, ythreshold, 19, ythreshold);
+  //
 
-  document.getElementById("calculations"). innerHTML = "Speed = " + speed + "<br>" + "Time = " + timeDiff + "<br> " + "StartTtime =" + startTime + "<br> "  + "EndTime =" + endTime  ;
+
+
+
 }
