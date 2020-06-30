d85188f5f30aca9f5ec33354efdc5378de07118c
MichaelDies <52653092+MichaelDies@users.noreply.github.com>
2020-06-30 21:27:25 +0300

    Update sketch.js

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,18 +1,19 @@
 let input;
 var startTime, endTime;
-var counter = 0;
+this.counter = 0;
 var timeDiff;
 var speed;
 var energy;
-var max ;
-var max1;
-var max3;
+this.max = 0.10 ;
+var volume;
+this.threshold=0.1;
 // function myFunction() {
 //   var table = document.getElementById("myRow");
 //   var row = table.insertRow(0);
 //   var cell1 = row.insertCell(0);
 //   row.innerHTML = "NEW CELL1";
 // }
+
 
 
 function start(){
@@ -30,56 +31,47 @@ function setup() {
 }
 
 function draw() {
-    let volume = input.getLevel();
-    let threshold = 0.20;
-    max3 = volume;
-    if (counter == 0){
-       if (volume > threshold) {
-         max = volume;
-         if (max > volume) {
-           volume = max;
-         }
-         else {
-           max = volume;
-         }
-            if (max = volume) {
-              startTime = Date.now()/1000;
-              counter = 1;
-            }
+
+    setInterval(()=>{
+
+
+      volume = input.getLevel();
+
+    //max = volume;
+    if (volume > threshold) {
+      if (counter == 0){ // first measurment , first sound
+        if (max > volume) {
+            startTime = Date.now()/1000;
+            counter = 1;
+            max = 0.10;
+        }
+        else {
+          max = volume;
+        }
+      }
+        else { // second measurment , second sound, and calculations.
+          if (max > volume) {
+            endTime = Date.now()/1000;
+            timeDiff = endTime - startTime;
+              input.stop();
+                  speed = 10 / timeDiff; // the target is suppost to be at 10 meters, because speed is distance / time.
+                  energy = (1/2) * 0.2 * (speed * speed); //
+                  document.getElementById("calculations"). innerHTML = "Speed = " + speed + "  (KM/S)"
+                                                             + "<br>" + "Time = " + timeDiff + "  (Seconds)"
+                                                             + "<br>" + "Energy = " + energy + "  (Jol)"
+                                                             + "<br>" + "StartTtime = " + startTime + "  (Seconds)"
+                                                             + "<br>" + "EndTime = " + endTime +"  (Seconds)" ;
+                    counter =0;
+                  //location.reload();
+          }
+          else {
+            max = volume;
+          }
         }
       }
 
-       else if  (counter == 1){
-          if (volume > threshold) {
-            max1 = volume;
-            if (max1 > volume) {
-              volume = max1;
-            }
-            else {
-              max1 = volume;
-            }
-            if (max1 = volume) {
-              if (max > max1) {
-              endTime = Date.now()/1000;
-              timeDiff = endTime - startTime;
-              counter = 2;
-                    }
-                  }
-                }
-              }
-              else  if (counter == 2){
-                  if (timeDiff> 0) {
-                        speed = 10 / timeDiff;
-                        energy = (1/2) * 0.15 * (speed * speed);
-                      alert( "Speed = " + speed + "  (KM/S)" +
-                          "Time = " + timeDiff + "  (Seconds)"
-                          + "Energy = " + energy + "  (Jol)"
-                          +"StartTtime = " + startTime + "  (Seconds)"
-                          + "EndTime = " + endTime +"  (Seconds)" );
-                        location.reload();
-                        counter = 0;
-                        }
-                      }
+    },200);
+
 //counter = 0;
   let y = map(volume, 0, 1, height, 0);
   let ythreshold = map(threshold, 0, 1, height, 0);
