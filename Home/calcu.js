c51e81a5788c4cc69935ed5c41047ae2c18f1176
MichaelDies <52653092+MichaelDies@users.noreply.github.com>
2020-08-19 15:39:40 +0300

    Update calcu.js

--- Home/calcu.js
+++ Home/calcu.js
@@ -1,8 +1,3 @@
-var brick1;
-var wall1;
-
-
-
 
 function Calculate(){
 	
@@ -10,27 +5,42 @@ function Calculate(){
   wall_h = document.getElementById("h1").value;
   brick_l = document.getElementById("l").value;
   brick_h = document.getElementById("h").value;
+  window_l = document.getElementById("wl").value;
+  window_h = document.getElementById("wh").value;
+  
 
   wall_l_floated = parseFloat(wall_l);
   wall_h_floated = parseFloat(wall_h);
   brick_h_floated = parseFloat(brick_h);
   brick_l_floated = parseFloat(brick_l);
 
-//   if (wall_l_floated>0) {
-//     if (brick_l_floated>0) {
-    
-//   }
-//   else {
-//       alert("The Distance or the Weight is Zero ...");
-//     }
-// }
-// else {
-//     alert("The Distance or the Weight is Zero ...");
-//   }
+ 
 
 var brick = wall_l / brick_l;
 var brick1 = wall_h / brick_h;
 var all = brick * brick1;
-document.getElementById("calculations"). innerHTML = "Bricks Needed in Width= " + brick + "<br>" + "Bricks Needed In Hight= " + brick1 + "<br>" + "Bricks Needed All= " + all   ;
 
+
+var window1 = window_l / brick_l;
+var window2 = window_h / brick_h;
+var window_f = window1 * window2;
+
+var everything =  all - window_f;
+
+
+document.getElementById("calculations"). innerHTML = "Bricks Needed in Width= " + brick 
+													+ "<br>" + "Bricks Needed In Hight= " + brick1 
+													+ "<br>" + "Bricks Needed All= " + all
+													+ "<br>" + "Bricks Needed in hight= " + window2 
+													+ "<br>" + "Bricks Needed in width= " + window1 
+													+ "<br>" + "Bricks Needed for window= " + window_f 
+													+ "<br>" + "All the Bricks Needes = " + everything; 
+
+}
+
+function window(){
+
+	for (var i = n; i >= n; i++) {
+		n++;
+	}
 }
