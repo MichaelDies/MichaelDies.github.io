648a576f147c13cbe63f0cbf9b7df204ebc59564
MichaelDies <52653092+MichaelDies@users.noreply.github.com>
2021-01-04 21:42:12 +0200

    Add files via upload

---
+++ Majd-Ja/app3.js
@@ -0,0 +1,28 @@
+const modal = document.querySelector(".modal");
+const previews = document.querySelectorAll(".container img");
+const original = document.querySelector(".full-img");
+const caption = document.querySelector(".caption");
+
+previews.forEach((preview) =>{
+    preview.addEventListener("click", () => {
+        modal.classList.add("open");
+        original.classList.add("open");
+        //change text and img everyclick
+        const originalSrc = preview.getAttribute("data-original");
+        original.src = `./cribs/${originalSrc}`;
+        const altText = preview.alt;
+        caption.textContent = altText;
+    });
+});
+
+modal.addEventListener("click", (e) => {
+    if(e.target.classList.contains("modal")){
+        modal.classList.remove("open");
+        original.classList.remove("open");
+    }
+});
+
+
+
+
+
