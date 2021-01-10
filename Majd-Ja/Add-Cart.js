19b4fe8890ac07b8a3b7ed7b17a587329ae39047
MichaelDies <52653092+MichaelDies@users.noreply.github.com>
2021-01-10 16:24:56 +0200

    Add files via upload

---
+++ Majd-Ja/Add-Cart.js
@@ -0,0 +1,122 @@
+let carts = document.querySelectorAll('.buy');
+let products =[
+    {
+        name: 'Cross1',
+        tag: '1' ,
+        price: 2,
+        dimentions :'L: 5.0cm  W: 4.5cm  H: 1.0cm',
+        inCart: 0
+    },
+    {
+        name: 'Cross2',
+        tag: '27' ,
+        price: 3,
+        dimentions :'L: 9.0cm  W: 5.0cm  H: 1.5cm',
+        inCart: 0
+    },
+    {
+        name: 'Cross3',
+        tag: '2' ,
+        price: 5,
+        dimentions :'L: 11.0cm  W: 6.0cm  H: 2.0cm',
+        inCart: 0
+    }
+];
+
+for (let i=0; i < carts.length; i++){
+    carts[i].addEventListener('click', ()=>{
+        cartNumbers(products[i]);
+        totalCost(products[i]);
+    })
+}
+
+function onLoadCartNumbers(){
+    let productNumbers = localStorage.getItem('cartNumbers');
+    if(productNumbers){
+        document.querySelector('.cart span').textContent = productNumbers;
+    }
+}
+
+function cartNumbers(product) {
+    let productNumbers = localStorage.getItem('cartNumbers');
+    productNumbers = parseInt(productNumbers);
+    if(productNumbers){
+        localStorage.setItem('cartNumbers' , productNumbers + 1);
+        document.querySelector('.cart span').textContent = productNumbers + 1;
+    }else{
+        localStorage.setItem('cartNumbers' , 1); 
+        document.querySelector('.cart span').textContent = 1;
+    }
+    setItems(product);
+}
+
+function setItems(product){
+    let cartItems = localStorage.getItem('productsInCart');
+    cartItems = JSON.parse(cartItems);
+
+    if(cartItems != null){
+        if(cartItems[product.tag] == undefined){
+            cartItems = {
+                ...cartItems,
+                [product.tag] : product
+            }
+        }
+        cartItems[product.tag].inCart += 1; 
+    }else{
+        product.inCart = 1;
+        cartItems = {
+            [product.tag]: product
+        } 
+    }
+    localStorage.setItem("productsInCart", JSON.stringify(cartItems));
+}
+
+function totalCost(product){
+    let cartCost = localStorage.getItem('totalCost');
+    if(cartCost != null ){
+        cartCost = parseInt(cartCost);
+        localStorage.setItem("totalCost", cartCost + product.price);
+    }else{
+        localStorage.setItem("totalCost" , product.price);
+    }
+}
+
+function displayCart(){
+    let cartItems = localStorage.getItem("productsInCart");
+    cartItems = JSON.parse(cartItems);
+    let productContainer = document.querySelector(".products");
+    let cartCost = localStorage.getItem('totalCost');
+
+
+    console.log(cartItems);
+    if(cartItems && productContainer ) {
+        
+        Object.values(cartItems).map(item => {
+            productContainer.innerHTML += `
+            <div class="product">
+                <ion-icon name="close-circle-outline" class="delete"></ion-icon>
+                <img src="./Crosses-Main/${item.tag}.jpg" style="max-width: 100px;">
+            </div>
+            <div class="price"> $${item.price},00</div>
+            <div class="quantity">
+            <ion-icon name="arrow-back-circle-outline"></ion-icon>
+            <span>  ${item.inCart}  </span>
+            <ion-icon name="arrow-forward-circle-outline"></ion-icon>
+            </div>
+            <div class="total">
+                $${item.inCart * item.price},00
+            </div>            
+            `;
+        });
+
+        productContainer.innerHTML += `
+        <div class="basketTotalContainer">
+            <h4 class="basketTotalTitle"> Basket Total </h4>
+            <h4 class="basketTotal">$${cartCost},00 </h4>
+        </div>
+        `;
+    }; 
+};
+
+onLoadCartNumbers();
+displayCart();
